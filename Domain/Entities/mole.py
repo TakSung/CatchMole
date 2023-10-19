971673c7023dc13f8b499af3dbcbcf15a252e7f2
taktakgyun <vbmrkk@naver.com>
2023-10-19 17:20:45 +0900

    log : IBoard, IRaiseObj && Add : Common/ObjectType.py get_object_type(i:int)->ObjectType

--- Domain/Entities/mole.py
+++ Domain/Entities/mole.py
@@ -9,6 +9,7 @@ import time
 class Mole(IRaiseObj, IMoleSubject):
 
     # 두더지가 생성된 시점부터 일어나 있음 -> state
+    # 두더지가 특정 시간이 지나면 저절로 아래로 내려감
     def __init__(self, observer: IMoleObserver, timer: int = 2):
         self.state = True  # raise
         self.type = ObjectType.BASIC_MOLE
---
+++ TEST/Common/test_object_type.py
@@ -0,0 +1,36 @@
+import __init__
+import unittest
+import sys
+from Common.ObjectType import *
+
+class test_object_type(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = 'test'
+        print(sys._getframe(0).f_code.co_name)
+
+    @classmethod
+    def tearDownClass(cls):
+        "Hook method for deconstructing the class fixture after running all tests in the class."
+        print(sys._getframe(0).f_code.co_name)
+
+    def setUp(self):
+        "Hook method for setting up the test fixture before exercising it."
+        print('\t', sys._getframe(0).f_code.co_name)
+
+    def tearDown(self):
+        "Hook method for deconstructing the test fixture after testing it."
+        print('\t', sys._getframe(0).f_code.co_name)
+
+    def test_start(self):
+        print('\t\t', sys._getframe(0).f_code.co_name)
+        type = get_object_type(0)
+        self.assertEqual(ObjectType.NONE, type)
+        type = get_object_type(1)
+        self.assertEqual(ObjectType.BASIC_MOLE, type)
+            
+
+
+
+if __name__ == '__main__':
+    unittest.main()
